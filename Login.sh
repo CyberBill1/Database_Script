bbacf7ffb2da59f3b65ff0960280975a8ead5f19
Ede Ifeanyichukwu Barth <104857012+CyberBill1@users.noreply.github.com>
2022-10-18 03:06:13 +0100

    Update Login.sh

--- Login.sh
+++ Login.sh
@@ -1,30 +1,40 @@
 #! /bin/bash
 
-#This Log in Page Shell-script was created by Engr Ifeanyi Barth
-#This Bash Scripts creates a database of Usernames and Passwords of users of an app. It validates the Log in for and allows Access when details are correct while deny>
+#Shell Script Designed by Ifeanyi Barth
+
+#This Bash Scripts creates a database of Usernames and Passwords of users of an app. 
+#It validates the Log in for and allows Access when details are found while denying access to users not found in the database 
 
 
 #Creating the Database by declaring Variables
 
 #Usernames
-m=a
-n=b
-u=c
+m=John
+n=Janet
+u=Jack
+
 #Passwords
-o=1
-p=2
-v=3
+o=Johnpass
+p=Janetpass
+v=Jackpass
 
 #Other Variables
+echo "Welcome to the portal"
+sleep 1
+echo
+read -p "What is your name?  " name
 
-w="Login successful, welcome. What will you like to do today" $(date)"?"
+echo
+w=(Login successful.  Welcome $name,  Today is " $(date)".  How may we serve you?)
+z=(Processing...)
+x=(Access denied as your data was not found in our database)
 
 #Read statements to accept input from users and store them in "User" and "Pass" files respectively
 
 read -p "Enter username:" us
-
-read -p "Enter password:" pa
-
+echo
+read -s -p  "Enter password:" pa
+# -s ensures passwords remains hidden when typing
 #Checking conditions for User M
 if [ $us == $m ];
 
@@ -32,10 +42,18 @@ then
 
 if [ $pa == $o ];
 then
-echo  $w
+echo
+echo $z
+
+sleep 2
+echo
+echo  ${w[*]}
 
 else
-echo "Access Denied"
+echo $z
+sleep 2
+echo
+echo ${x[*]}
 
 fi
 fi
@@ -45,10 +63,18 @@ if [ $us == $n ];
 then
 if [ $pa == $p ];
 then
-echo $w
+echo
+echo $z
+sleep 2
+echo
+echo ${w[*]}
 
 else
-echo "Access denied"
+echo
+echo $z
+sleep 2
+echo
+echo ${x[*]}
 fi
 fi
 
@@ -60,10 +86,17 @@ then
 
 if [ $pa == $v ];
 then
-echo $w
+echo
+echo $z
+sleep 2
+echo
+echo ${w[*]}
 
 else
-echo "Access Denied"
+echo $z
+sleep 2
+echo
+echo ${x[*]}
 
 fi
 fi
